67c2d91c96e7795a0ef02c07aaf8c40a3fe110dc
Melvin Lee <melvin.lee@iswish.nl>
2023-04-03 08:34:27 +0200

    chore(reisdocumentservice): setup reisdocumenten endpoint

---
+++ src/ReisdocumentService/Controllers/ReisdocumentController.cs
@@ -0,0 +1,30 @@
+﻿using HaalCentraal.ReisdocumentService.Generated;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaalCentraal.ReisdocumentService.Controllers;
+
+[ApiController]
+public class ReisdocumentController : Generated.ControllerBase
+{
+    private readonly ILogger<ReisdocumentController> _logger;
+
+    public ReisdocumentController(ILogger<ReisdocumentController> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<ActionResult<ReisdocumentenQueryResponse>> Reisdocumenten([FromBody] ReisdocumentenQuery body)
+    {
+        _logger.LogDebug("Request headers: {@headers}", HttpContext.Request.Headers);
+        _logger.LogDebug("Request body: {@body}", body);
+
+        var retval = body switch
+        {
+            _ => new ReisdocumentenQueryResponse()
+        };
+
+        _logger.LogDebug("Response body: {@responsebody}", retval);
+
+        return Ok(retval);
+    }
+}
