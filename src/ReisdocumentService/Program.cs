ddc63d2a0e9a2da459d4805998548f20f5feecb6
Melvin Lee <melvin.lee@iswish.nl>
2023-04-05 08:31:06 +0200

    chore(reisdocumentservice): setup serilog

--- src/ReisdocumentService/Program.cs
+++ src/ReisdocumentService/Program.cs
@@ -2,8 +2,25 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using HaalCentraal.ReisdocumentService.Extensions;
 using HaalCentraal.ReisdocumentService.Validators;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog((context, config) =>
+{
+    config
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.WithExceptionDetails()
+        .Enrich.FromLogContext()
+        .Enrich.With<ActivityEnricher>()
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                         theme: AnsiConsoleTheme.Code)
+        .WriteTo.Seq(context.Configuration["Seq:ServerUrl"]);
+});
 
 // Add services to the container.
 
