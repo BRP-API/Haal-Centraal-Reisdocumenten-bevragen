bdbda5078cfd47593f17de1814432369990e24a2
Melvin Lee <melvin.lee@iswish.nl>
2023-04-17 14:47:51 +0200

    feat(brpproxy): GBA -> HC mappers

---
+++ src/ReisdocumentProxy/Profiles/RaadpleegMetReisdocumentnummerResponseProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HC = HaalCentraal.ReisdocumentProxy.Generated;
+using Gba = HaalCentraal.ReisdocumentProxy.Generated.Gba;
+
+namespace ReisdocumentProxy.Profiles;
+
+public class RaadpleegMetReisdocumentnummerResponseProfile : Profile
+{
+    public RaadpleegMetReisdocumentnummerResponseProfile()
+    {
+        CreateMap<Gba.RaadpleegMetReisdocumentnummerResponse, HC.RaadpleegMetReisdocumentnummerResponse>();
+    }
+}
