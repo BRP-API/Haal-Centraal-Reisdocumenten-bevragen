9ee45638cd58fa536127d3726f99b0f57ec0848d
Melvin Lee <melvin.lee@iswish.nl>
2023-04-20 08:35:16 +0200

    chore(automation): stap definitie toegevoegd

--- features/step_definitions/step_defs.js
+++ features/step_definitions/step_defs.js
@@ -223,6 +223,15 @@ Then(/^heeft het object de volgende '(.*)' gegevens$/, function (gegevensgroep, 
     this.context.expected[gegevensgroep] = dataTable.hashes();
 });
 
+Then(/^heeft de response een '(\w*)' zonder gegevens$/, function (_) {
+    this.context.verifyResponse = true;
+    this.context.leaveEmptyObjects = true;
+
+    if(this.context.expected === undefined) {
+        this.context.expected = [ {} ];
+    }
+});
+
 After({tags: 'not @fout-case'}, function() {
 
     this.context.response.status.should.equal(200, `response body: ${JSON.stringify(this.context.response.data, null, '\t')}`);
