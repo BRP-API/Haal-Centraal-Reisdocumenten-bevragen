aa1cf4d2adfac7866878722dec3cd53f3292d120
Melvin Lee <melvin.lee@iswish.nl>
2023-05-03 14:32:22 +0200

    chore(ci): uitvoeren specs-verify script

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-PARAMS="${1}"
-if [[ ! -n $PARAMS ]]; then
-    PARAMS={}
-fi
+DBHOST=$1
+DBUSER=$2
+DBPASSWORD=$3
+CLIENTID=$4
+CLIENTSECRET=$5
+
+PARAMS="{ \
+  \"poolConfig\": { \
+    \"host\": \"${DBHOST}\", \
+    \"user\": \"${DBUSER}\", \
+    \"password\": \"${DBPASSWORD}\" \
+  }, \
+  \"client\": { \
+    \"clientId\": \"${CLIENTID}\", \
+    \"clientSecret\": \"${CLIENTSECRET}\" \
+  }\
+}"
 
 mkdir -p docs/features
 
+npx cucumber-js -f json:docs/features/test-result-autorisatie-gba.json \
+            -f summary:docs/features/test-result-autorisatie-gba-summary.txt \
+            features \
+            --tags "not @skip-verify" --tags "@autorisatie" \
+            --world-parameters "$PARAMS"
+
+npx cucumber-js -f json:docs/features/test-result-protocollering-gba.json \
+            -f summary:docs/features/test-result-protocollering-gba-summary.txt \
+            features \
+            --tags "not @skip-verify" --tags "@protocollering" \
+            --world-parameters "$PARAMS"
+
 npx cucumber-js -f json:docs/features/test-result-zoeken-en-raadplegen-gba.json \
             -f summary:docs/features/test-result-zoeken-en-raadplegen-gba-summary.txt \
-            features/raadpleeg-met-reisdocumentnummer \
-            --tags "not @skip-verify" --tags "@gba" \
+            features/ \
+            --tags "not @skip-verify" --tags "@gba" --tags "not @autorisatie" --tags "not @protocollering" \
             --world-parameters "$PARAMS"
 
 # cucumber-js -f json:docs/features/test-result-zoeken-en-raadplegen.json \
