bf3ac564028afbca134751e7830a803e943fa68c
Melvin Lee <melvin.lee@iswish.nl>
2023-07-11 14:13:38 +0200

    fix(mock): gemeenteVanInschrijving input parameter verwijderd

--- src/ReisdocumentService/Repositories/ReisdocumentQueryExtensions.cs
+++ src/ReisdocumentService/Repositories/ReisdocumentQueryExtensions.cs
@@ -7,14 +7,12 @@ public static class ReisdocumentQueryExtensions
     public static Specification<GbaReisdocument> ToSpecification(this RaadpleegMetReisdocumentnummer query)
     {
         return new ReisdocumentnummerSpecification(query.Reisdocumentnummer)
-            .And(new GemeenteVanInschrijvingSpecification(query.GemeenteVanInschrijving!))
             ;
     }
 
     public static Specification<GbaReisdocument> ToSpecification(this ZoekMetBurgerservicenummer query)
     {
         return new BurgerservicenummerSpecification(query.Burgerservicenummer)
-            .And(new GemeenteVanInschrijvingSpecification(query.GemeenteVanInschrijving!))
             ;
     }
 }
