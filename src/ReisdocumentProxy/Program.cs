dd27e4c5893f3538e2375d6debaf322afdd5ebf9
Melvin Lee <melvin.lee@iswish.nl>
2023-07-11 10:47:10 +0200

    feat(proxy): structured logging mbv serilog

--- src/ReisdocumentProxy/Program.cs
+++ src/ReisdocumentProxy/Program.cs
@@ -1,9 +1,30 @@
+using Elastic.Apm.SerilogEnricher;
+using Elastic.CommonSchema.Serilog;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using ReisdocumentProxy.DelegatingHandlers;
 using ReisdocumentProxy.Middlewares;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Host.UseSerilog((context, config) =>
+{
+    config
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.WithElasticApmCorrelationInfo()
+        .Enrich.WithExceptionDetails()
+        .Enrich.FromLogContext()
+        .Enrich.With<ActivityEnricher>()
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                         theme: AnsiConsoleTheme.Code)
+        .WriteTo.File(new EcsTextFormatter(), context.Configuration["Ecs:Path"]!)
+        .WriteTo.Seq(context.Configuration["Seq:ServerUrl"]!);
+});
 
 builder.Configuration.AddJsonFile(Path.Combine("configuration", "ocelot.json"))
                      .AddJsonFile(Path.Combine("configuration", $"ocelot.{builder.Environment.EnvironmentName}.json"), true)
