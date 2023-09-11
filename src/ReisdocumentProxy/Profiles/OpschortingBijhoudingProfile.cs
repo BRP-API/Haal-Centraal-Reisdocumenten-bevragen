1d7e976106da621a87ff19ec3a8be996ef01110a
Melvin Lee <melvin.lee@iswish.nl>
2023-09-11 11:41:56 +0200

    fix(profile): opschorting bijhouding mapping

---
+++ src/ReisdocumentProxy/Profiles/OpschortingBijhoudingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ReisdocumentProxy.Mappers;
+using Gba = HaalCentraal.ReisdocumentProxy.Generated.Gba;
+using HC = HaalCentraal.ReisdocumentProxy.Generated;
+
+namespace ReisdocumentProxy.Profiles;
+
+public class OpschortingBijhoudingProfile : Profile
+{
+    public OpschortingBijhoudingProfile()
+    {
+        CreateMap<Gba.GbaOpschortingBijhouding, HC.OpschortingBijhouding>()
+            .ForMember(dest => dest.Datum, opt => opt.MapFrom(src => src.Datum.Map()))
+            ;
+    }
+}
