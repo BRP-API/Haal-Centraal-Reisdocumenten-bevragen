60a56fb3eaea0dc2b6c8d06cbc695d7c2bdfcfb2
Melvin Lee <melvin.lee@iswish.nl>
2024-02-01 13:48:01 +0100

    chore(proxy): 'proxy naar proxy aanroep check' gewijzigd

--- src/ReisdocumentProxy/Helpers/TransformHelper.cs
+++ src/ReisdocumentProxy/Helpers/TransformHelper.cs
@@ -8,28 +8,36 @@ namespace ReisdocumentProxy.Helpers;
 
 public static class TransformHelper
 {
+    private static bool IsProxyResponse(this string payload)
+    {
+        var proxyTokens = new List<string>
+        {
+                "langFormaat",
+                "\"geheimhoudingPersoonsgegevens\":true"
+        };
+
+        return proxyTokens.Exists(t => payload.Contains(t));
+    }
+
     public static string Transform(this string payload, IMapper mapper, List<string> fields)
     {
-        try
+        if (payload.IsProxyResponse())
         {
-            var response = JsonConvert.DeserializeObject<Gba.ReisdocumentenQueryResponse>(payload);
-
-            var fieldsToReturn = fields.AddExtraReisdocumentFields();
-
-            ReisdocumentenQueryResponse retval = response switch
-            {
-                Gba.RaadpleegMetReisdocumentnummerResponse r => mapper.Map<RaadpleegMetReisdocumentnummerResponse>(r).Filter(fieldsToReturn),
-                Gba.ZoekMetBurgerservicenummerResponse r => mapper.Map<ZoekMetBurgerservicenummerResponse>(r).Filter(fieldsToReturn),
-                _ => throw new NotSupportedException(),
-            };
-
-            return retval.ToJsonWithoutNullAndDefaultValues();
-        }
-        catch(JsonReaderException)
-        {
-            // payload komt van de reisdocumenten API. As-is doorzetten
             return payload;
         }
+
+        var response = JsonConvert.DeserializeObject<Gba.ReisdocumentenQueryResponse>(payload);
+
+        var fieldsToReturn = fields.AddExtraReisdocumentFields();
+
+        ReisdocumentenQueryResponse retval = response switch
+        {
+            Gba.RaadpleegMetReisdocumentnummerResponse r => mapper.Map<RaadpleegMetReisdocumentnummerResponse>(r).Filter(fieldsToReturn),
+            Gba.ZoekMetBurgerservicenummerResponse r => mapper.Map<ZoekMetBurgerservicenummerResponse>(r).Filter(fieldsToReturn),
+            _ => throw new NotSupportedException(),
+        };
+
+        return retval.ToJsonWithoutNullAndDefaultValues();
     }
 
     private static RaadpleegMetReisdocumentnummerResponse Filter(this RaadpleegMetReisdocumentnummerResponse src, IEnumerable<string> fields)
